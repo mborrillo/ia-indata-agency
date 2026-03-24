2e619d851223f14250d52f2e6d77431abcfe9bec
Marcos  Borrillo <68borrismar@gmail.com>
2026-03-24 16:57:03 +0100

    Update app.py

--- projects/energia-mercados-hosteleria/app.py
+++ projects/energia-mercados-hosteleria/app.py
@@ -107,7 +107,7 @@ html, body, .stApp,
 /* Tabs / Navegación fija */
 [data-testid="stTabs"] [role="tablist"] { 
     position: fixed;
-    top: 100px; /* Altura donde termina el header */
+    top: 97px; /* Altura donde termina el header */
     left: 0;
     right: 0;
     z-index: 999;
